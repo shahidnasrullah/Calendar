2738d9bee0d08bb508ae9fb4c45d6fbd3cf0bd43
SHAHID <shahid.nasrullah@yahoo.com>
2013-03-11 13:14:33 +0500

    Initial Commit

---
+++ CalendarView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CalendarView
+//
+//  Created by coeus on 11/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CalendarView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CalendarView
+//
+//  Created by coeus on 11/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
